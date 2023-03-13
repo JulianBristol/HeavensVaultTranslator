878c6c2cacf7c4637cecad7288d1aa3c7dc2d44f
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-03-13 18:44:40 -0400

    Add Connection to database

--- src/main/java/com/example/heavensvaulttranslator/DatabaseConnection.java
+++ src/main/java/com/example/heavensvaulttranslator/DatabaseConnection.java
@@ -1,4 +1,14 @@
 package com.example.heavensvaulttranslator;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/heavens_vault_translator";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+    public Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
